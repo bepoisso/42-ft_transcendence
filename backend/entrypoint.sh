4ed7ea491c23bd56a370e1aff70b018556894979
bepoisso <poisson.benjamin69@gmail.com>
2025-08-04 18:54:46 +0200

    FIX feat(db): error generate db

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,10 +3,12 @@
 DB_FILE="/app/back.db"
 if [ ! -f "$DB_FILE" ]; then
 	# sqlite3 "$DB_FILE" "VACUUM;"
-	sqlite3 "$DB_FILE" < /tmp/init.sql && rm /tmp/init.sql
-	echo "Database '$DB_FILE' created in /app."
-else
-	echo "Database '$DB_FILE' already exists in /app."
+	sqlite3 "$DB_FILE" < /tmp/init.sql && rm /tmp/init.sql+32
+   	echo "Database '$DB_FILE' created in /app."
+el< <se
+	e0.
+<	
+	.cho "Database '$DB_FILE' already exists in /app."
 fi
 
 tail -f /dev/null
