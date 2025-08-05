c20094b0a293a56e3b2f93c4230e641bd8c36c50
bepoisso <poisson.benjamin69@gmail.com>
2025-08-05 15:13:16 +0200

    update feat(docker): update volume andling

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-DB_FILE="/app/back.db"
+DB_FILE="/bdd/back.db"
 
 if [ ! -f "$DB_FILE" ]; then
 	# Création de la base de données à partir du script SQL
 	sqlite3 "$DB_FILE" < /tmp/init.sql && rm /tmp/init.sql
-	echo "✅ Database '$DB_FILE' created in /app."
+	echo "✅ Database '$DB_FILE' created in /bdd."
 else
-	echo "ℹ️ Database '$DB_FILE' already exists in /app."
+	echo "ℹ️ Database '$DB_FILE' already exists in /bdd."
 fi
 
 # Démarrage du serveur Nginx
