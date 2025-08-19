c2fad62ae62ce18cbe94c441de78a8f2672c7c7a
bepoisso <poisson.benjamin69@gmail.com>
2025-08-19 18:58:46 +0200

    UPDATE db

--- backend/src/db/setup.sql
+++ backend/src/db/setup.sql
@@ -5,10 +5,35 @@ CREATE TABLE users (
 	username TEXT NOT NULL UNIQUE,
 	email TEXT NOT NULL UNIQUE,
 	password_hash TEXT,
+	avatar_url TEXT,
+	games_played NUMBER DEFAULT 0,
+	games_won NUMBER DEFAULT 0,
+	is_connected NUMBER DEFAULT 0,
 	google_id TEXT UNIQUE,
 	twofa_enable BOOLEAN DEFAULT 0,
-	twofa_secret TEXT UNIQUE
+	twofa_secret TEXT UNIQUE,
+	friend_list TEXT
 );
 
+DROP TABLE IF EXISTS games;
 
--- IL faut ajouter : avatarURL (string), games played (string), games won (string), friends (list), isConnected (boolean)
+CREATE TABLE games (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	player_id_left NUMBER NOT NULL,
+	player_id_right NUMBER,
+	player_id_won NUMBER,
+	game_date TEXT,
+	score TEXT,
+	tournament_id NUMBER DEFAULT 0,
+);
+
+DROP TABLE IF EXISTS tournament;
+
+CREATE TABLE tournament (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	tournament_name TEXT NOT NULL,
+	nbr_player NUMBER NOT NULL,
+	player_list TEXT,
+	player_won NUMBER,
+	tournament_date TEXT
+);
